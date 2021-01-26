965cc8d6c5d88152e846d9199d3cf8f349edfa68
bolucat <75832646+bolucat@users.noreply.github.com>
2021-01-27 00:12:24 +0800

    rebuild Gitea

--- src/build/gitea.sh
+++ src/build/gitea.sh
@@ -5,15 +5,31 @@ CUR=$PWD
 VERSION=$(wget -qO- https://api.github.com/repos/go-gitea/gitea/releases/latest | grep 'tag_name' | cut -d\" -f4 | head -1)
 
 # Start Build
+mkdir -p ${CUR}/release 
 git clone https://github.com/go-gitea/gitea && cd gitea
 git branch -a && git checkout ${VERSION}
 
 npx browserslist@latest --update-db
-TAGS="bindata sqlite sqlite_unlock_notify" make build
-mkdir -p ${CUR}/release && mv gitea ${CUR}/release/gitea_amd64
 
-CC=aarch64-unknown-linux-gnu-gcc GOOS=linux GOARCH=arm64 TAGS="bindata sqlite sqlite_unlock_notify" make build
-mv gitea ${CUR}/release/gitea_arm64 && chmod +x ${CUR}/release/*
+TAG="bindata sqlite sqlite_unlock_notify"
+
+ARCHS=( 386 amd64 arm arm64 ppc64le s390x )
+ARMS=( 6 7 )
+
+for ARCH in ${ARCHS[@]}; do
+	if [ "${ARCH}" == "arm" ]; then
+		for ARM in ${ARMS[@]}; do
+			echo "Building gitea-linux-${ARCH}32-v${ARM}"
+			GOOS=linux GOARCH=${ARCH} GOARM=${ARM} TAGS=${TAG} make build
+			mv gitea ${CUR}/release/gitea-linux-${ARCH}32-v${ARM}
+		done
+	else
+		echo "Building gitea-linux-${ARCH}"
+		GOOS=linux GOARCH=${ARCH} TAGS=${TAG} make build
+		mv gitea ${CUR}/release/gitea-linux-${ARCH}
+	fi
+done
 
 # Clean
+chmod +x ${CUR}/release/*
 cd ${CUR} && rm -rf gitea
