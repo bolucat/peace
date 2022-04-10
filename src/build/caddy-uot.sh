72325c33aee43c2da8ace016ec6afe10255bf60b
bolucat <hglkr@pm.me>
2022-04-10 09:56:04 +0800

    add caddy sagernet uot support

---
+++ src/build/caddy-uot.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Set variables
+QUIC=$(wget -qO- https://api.github.com/repos/lucas-clemente/quic-go/tags | grep 'name' | cut -d\" -f4 | head -1)
+PROTOCOL=$(wget -qO- https://api.github.com/repos/mastercactapus/caddy2-proxyprotocol/tags | grep 'name' | cut -d\" -f4 | head -1)
+
+# Get source code
+go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+git clone -b naive https://github.com/SagerNet/forwardproxy
+git clone https://github.com/mholt/caddy-l4 caddy-l4
+# git clone https://github.com/caddyserver/nginx-adapter
+git clone https://github.com/caddy-dns/cloudflare cloudflare
+git clone https://github.com/mholt/caddy-webdav caddy-webdav
+git clone -b ${QUIC} https://github.com/lucas-clemente/quic-go
+git clone -b ${PROTOCOL} https://github.com/mastercactapus/caddy2-proxyprotocol
+
+# Start Build
+NAIVE="--with github.com/caddyserver/forwardproxy=$PWD/forwardproxy"
+LAYER_4="--with github.com/mholt/caddy-l4=$PWD/caddy-l4"
+CF_DNS="--with github.com/caddy-dns/cloudflare=$PWD/cloudflare"
+WEBDAV="--with github.com/mholt/caddy-webdav=$PWD/caddy-webdav"
+QUIC_GO="--with github.com/lucas-clemente/quic-go=$PWD/quic-go"
+# NGINX="--with github.com/caddyserver/nginx-adapter=$PWD/nginx-adapter"
+PROTOCOLS="--with github.com/mastercactapus/caddy2-proxyprotocol=$PWD/caddy2-proxyprotocol"
+
+ARCHS=( 386 amd64 arm arm64 ppc64le s390x )
+ARMS=( 6 7 )
+
+mkdir -p release
+
+for ARCH in ${ARCHS[@]}; do
+	if [ "${ARCH}" == "arm" ]; then
+		for ARM in ${ARMS[@]}; do
+			echo "Building caddy-linux-uot-${ARCH}32-v${ARM}"
+			env GOOS=linux GOARCH=${ARCH} GOARM=${ARM} $GOPATH/bin/xcaddy build HEAD --output release/caddy-linux-uot-${ARCH}32-v${ARM} ${NAIVE} ${LAYER_4} ${CF_DNS} ${WEBDAV} ${QUIC_GO} ${PROTOCOLS}
+		done
+	else
+		echo "Building caddy-linux-uot-${ARCH}"
+		env GOOS=linux GOARCH=${ARCH} $GOPATH/bin/xcaddy build HEAD --output release/caddy-linux-uot-${ARCH} ${NAIVE} ${LAYER_4} ${CF_DNS} ${WEBDAV} ${QUIC_GO} ${PROTOCOLS}
+	fi
+done
---
+++ src/caddy-uot/caddy.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+PLATFORM=$1
+if [ -z "$PLATFORM" ]; then
+    ARCH="amd64"
+else
+    case "$PLATFORM" in
+        linux/386)
+            ARCH="386"
+            ;;
+        linux/amd64)
+            ARCH="amd64"
+            ;;
+        linux/arm/v6)
+            ARCH="arm32-v6"
+            ;;
+        linux/arm/v7)
+            ARCH="arm32-v7"
+            ;;
+        linux/arm64|linux/arm64/v8)
+            ARCH="arm64"
+            ;;
+        linux/ppc64le)
+            ARCH="ppc64le"
+            ;;
+        linux/s390x)
+            ARCH="s390x"
+            ;;
+        *)
+            ARCH=""
+            ;;
+    esac
+fi
+[ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
+
+# Download binary file
+CADDY_FILE="caddy-linux-uot-${ARCH}"
+
+echo "Downloading binary file: ${CADDY_FILE}"
+wget -O /usr/bin/caddy https://github.com/bolucat/peace/releases/latest/download/${CADDY_FILE} > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download binary file: ${CADDY_FILE}" && exit 1
+fi
+echo "Download binary file: ${CADDY_FILE} completed"
+
+chmod +x /usr/bin/caddy
