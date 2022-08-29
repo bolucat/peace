16fc1cede4e925a315039f264f0dfc3b35947bc0
Pocat <hglkr@pm.me>
2022-08-29 15:42:28 +0800

    add script to block bilibili tracker

---
+++ src/script/bilibili-trace-block.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        Bilibili Trace Block
+// @namespace   https://raw.githubusercontent.com/bolucat/peace/master/src/script/bilibili-trace-block.js
+// @match       *://*.bilibili.com/*
+// @run-at      document-start
+// @grant       unsafeWindow
+// @version     0.1
+// @author      Bolucat
+// @description Block Bilibili Trace
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    unsafeWindow.Sentry = {
+      init: () => {},
+      Integrations: { Vue: class {constructor() {}}}
+    }
+    var ops = unsafeWindow.history.pushState;
+    var ors = unsafeWindow.history.replaceState;
+    var callOriginal = (stateObj, title, url, calling) => {
+        if (new URL(`https://example.com${url}`).searchParams.get('vd_source') === null) {
+          calling(stateObj, title, url);
+        }
+    }
+    unsafeWindow.history.pushState = (stateObj, title, url) => callOriginal(stateObj, title, url, ops)
+    unsafeWindow.history.replaceState = (stateObj, title, url) => callOriginal(stateObj, title, url, ors)
+})();
