9e37a42bdd2eba6cedc67d7352695320f9eede31
bolucat <hglkr@pm.me>
2022-10-30 00:25:10 +0800

    add tuic

---
+++ src/tuic/tuic.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+PLATFORM=$1
+if [ -z "$PLATFORM" ]; then
+    ARCH="amd64"
+else
+    case "$PLATFORM" in
+        linux/amd64)
+            ARCH="amd64"
+            ;;
+        linux/arm/v6)
+            ARCH="arm32-v6"
+            ;;
+        linux/arm/v7)
+            ARCH="arm32-v7"
+            ;;
+        linux/arm64|linux/arm64/v8)
+            ARCH="arm64"
+            ;;
+        *)
+            ARCH=""
+            ;;
+    esac
+fi
+[ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
+
+# Download binary file
+TUIC_FILE="tuic-linux-${ARCH}"
+
+echo "Downloading binary file: ${TUIC_FILE}"
+wget -O $PWD/tuic https://github.com/bolucat/peace/releases/latest/download/${TUIC_FILE} > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download binary file: ${TUIC_FILE}" && exit 1
+fi
+echo "Download binary file: ${TUIC_FILE} completed"
+
+echo "Prepare to use"
+chmod +x tuic && mv tuic /usr/bin/tuic
