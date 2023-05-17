da287918ba36c7da0ce4adb3a9c16e4d90d21032
bolucat <hglkr@pm.me>
2023-05-17 08:47:22 +0800

    remove caddy2-proxyprotocol due to panic: module already registered: caddy.listeners.proxy_protocol

--- src/build/caddy-uot.sh
+++ src/build/caddy-uot.sh
@@ -2,7 +2,7 @@
 
 # Set variables
 # QUIC=$(wget -qO- https://api.github.com/repos/lucas-clemente/quic-go/tags | grep 'name' | cut -d\" -f4 | head -1)
-PROTOCOL=$(wget -qO- https://api.github.com/repos/mastercactapus/caddy2-proxyprotocol/tags | grep 'name' | cut -d\" -f4 | head -1)
+# PROTOCOL=$(wget -qO- https://api.github.com/repos/mastercactapus/caddy2-proxyprotocol/tags | grep 'name' | cut -d\" -f4 | head -1)
 
 # Get source code
 go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
@@ -12,7 +12,7 @@ git clone https://github.com/mholt/caddy-l4 caddy-l4
 git clone https://github.com/caddy-dns/cloudflare cloudflare
 git clone https://github.com/mholt/caddy-webdav caddy-webdav
 # git clone -b ${QUIC} https://github.com/lucas-clemente/quic-go
-git clone -b ${PROTOCOL} https://github.com/mastercactapus/caddy2-proxyprotocol
+# git clone -b ${PROTOCOL} https://github.com/mastercactapus/caddy2-proxyprotocol
 
 # Start Build
 NAIVE="--with github.com/caddyserver/forwardproxy=$PWD/forwardproxy"
@@ -21,7 +21,7 @@ CF_DNS="--with github.com/caddy-dns/cloudflare=$PWD/cloudflare"
 WEBDAV="--with github.com/mholt/caddy-webdav=$PWD/caddy-webdav"
 # QUIC_GO="--with github.com/lucas-clemente/quic-go=$PWD/quic-go"
 # NGINX="--with github.com/caddyserver/nginx-adapter=$PWD/nginx-adapter"
-PROTOCOLS="--with github.com/mastercactapus/caddy2-proxyprotocol=$PWD/caddy2-proxyprotocol"
+# PROTOCOLS="--with github.com/mastercactapus/caddy2-proxyprotocol=$PWD/caddy2-proxyprotocol"
 
 ARCHS=( 386 amd64 arm arm64 ppc64le s390x )
 ARMS=( 6 7 )
@@ -32,10 +32,10 @@ for ARCH in ${ARCHS[@]}; do
 	if [ "${ARCH}" == "arm" ]; then
 		for ARM in ${ARMS[@]}; do
 			echo "Building caddy-linux-uot-${ARCH}32-v${ARM}"
-			env GOOS=linux GOARCH=${ARCH} GOARM=${ARM} $GOPATH/bin/xcaddy build HEAD --output release/caddy-linux-uot-${ARCH}32-v${ARM} ${NAIVE} ${LAYER_4} ${CF_DNS} ${WEBDAV} ${PROTOCOLS}
+			env GOOS=linux GOARCH=${ARCH} GOARM=${ARM} $GOPATH/bin/xcaddy build HEAD --output release/caddy-linux-uot-${ARCH}32-v${ARM} ${NAIVE} ${LAYER_4} ${CF_DNS} ${WEBDAV}
 		done
 	else
 		echo "Building caddy-linux-uot-${ARCH}"
-		env GOOS=linux GOARCH=${ARCH} $GOPATH/bin/xcaddy build HEAD --output release/caddy-linux-uot-${ARCH} ${NAIVE} ${LAYER_4} ${CF_DNS} ${WEBDAV} ${PROTOCOLS}
+		env GOOS=linux GOARCH=${ARCH} $GOPATH/bin/xcaddy build HEAD --output release/caddy-linux-uot-${ARCH} ${NAIVE} ${LAYER_4} ${CF_DNS} ${WEBDAV}
 	fi
 done
